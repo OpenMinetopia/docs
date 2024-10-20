404b5676855dc19ca95270c480123fef702b4e95
Daan Plugge <35238334+TheBathDuck@users.noreply.github.com>
2024-10-20 16:27:49 +0200

    Docs base

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+	base: '/',
+	postcss: {
+		plugins: [
+			require('css-prefers-color-scheme/postcss'),
+		]
+	},
+	title: 'OpenMinetopia Documentation',
+	description: 'Documentatie voor de Open Source Minetopia Plugin!',
+	head: [
+		['link', { rel: 'icon', href: '/logo.png' }]
+	],
+	theme: "yuu",
+	themeConfig: {
+        yuu: {
+			defaultDarkTheme: true,
+		},
+		logo: '/logo.png',
+		nav: [
+			{ text: 'Discord', link: 'https://discord.gg/rfDp4RgZRD' }
+		],
+		sidebar: [
+			{
+				title: 'OpenMinetopia',
+				collapsable: false,
+				children: [
+					'/'
+				]
+			},
+			{
+				title: 'Functies',
+				collapsable: false,
+				children: [
+					'/features/banking',
+                    '/features/levels'
+				]
+			}
+		]
+	}
+}
